bc10d13e0319c03b972cb07ae517bb191df31c6e
Martin Botka <16738302+Haxk20@users.noreply.github.com>
2026-09-22 17:01:21 +0200

    search.c: Add correction margin to SE (#438)
    bench: 9005236

--- Source/search.c
+++ Source/search.c
@@ -1085,8 +1085,8 @@ static inline int16_t negamax(thread_t *thread, searchstack_t *ss,
     // No move beat tt score so we extend the search
     if (s_score < s_beta) {
       const int16_t double_margin =
-          SE_DOUBLE_MARGIN + SE_PV_DOUBLE_MARGIN * pv_node;
-      const int16_t triple_margin = SE_TRIPLE_MARGIN;
+          SE_DOUBLE_MARGIN + SE_PV_DOUBLE_MARGIN * pv_node - 20 * abs(correction) / 128;
+      const int16_t triple_margin = SE_TRIPLE_MARGIN - 25 * abs(correction) / 128;
       extensions++;
       extensions += s_score < s_beta - double_margin;
       extensions += s_score < s_beta - triple_margin;
